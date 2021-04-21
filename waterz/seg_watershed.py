7835dbf259b5c84b240daf85fcc74bfac1e1b7a8
donglaiw <donglai@boslogin01.rc.fas.harvard.edu>
2021-04-21 19:59:27 -0400

    update watershed

--- waterz/seg_watershed.py
+++ waterz/seg_watershed.py
@@ -3,7 +3,7 @@ import mahotas
 from scipy import ndimage
 
 def get_seeds(boundary, method='grid', next_id = 1,
-             seed_distance = 10, boundary_thres = 0.5):
+             seed_distance = 10, boundary_thres = 0.5, label_nb = None):
     if method == 'grid':
         height = boundary.shape[0]
         width  = boundary.shape[1]
@@ -15,31 +15,31 @@ def get_seeds(boundary, method='grid', next_id = 1,
         seeds = np.zeros_like(boundary).astype(np.int32)
         seeds[seed_positions] = np.arange(next_id, next_id + num_seeds).reshape((num_seeds_y,num_seeds_x))
 
-    if method == 'minima':
-        minima = mahotas.regmin(boundary)
-        seeds, num_seeds = mahotas.label(minima)
-        seeds += next_id
-        seeds[seeds==next_id] = 0
+    if method in ['minima', 'maxima_distance']:
+        if method == 'minima':
+            peak = mahotas.regmin(boundary)
+        elif method == 'maxima_distance':
+            # distance = mahotas.distance(boundary < boundary_thres)
+            distance = ndimage.morphology.distance_transform_cdt(boundary < boundary_thres)
+            peak = mahotas.regmax(distance)
+        if label_nb is None:
+            seeds, num_seeds = mahotas.label(peak)
+        else:
+            seeds, num_seeds = mahotas.label(peak, label_nb)
 
-    if method == 'maxima_distance':
-        distance = mahotas.distance(boundary < boundary_thres)
-        maxima = mahotas.regmax(distance)
-        seeds, num_seeds = mahotas.label(maxima)
-        seeds += next_id
-        seeds[seeds==next_id] = 0
+        seeds[seeds > 0] += next_id
+
     return seeds, num_seeds
 
-def watershed(affs, seed_method, use_mahotas_watershed = True, boundary_thres = 0.5):
-    affs_xy = 1.0 - 0.5*(affs[1] + affs[2])
-    depth  = affs_xy.shape[0]
+def watershed(affs, seed_method, boundary_thres = 0.5, label_nb = None, seg_bg = True):
     fragments = np.zeros_like(affs[0]).astype(np.uint64)
-    next_id = 1
+    depth  = fragments.shape[0]
+    next_id = 0
     for z in range(depth):
-        seeds, num_seeds = get_seeds(affs_xy[z], next_id=next_id, method=seed_method, boundary_thres=boundary_thres)
-        if use_mahotas_watershed:
-            fragments[z] = mahotas.cwatershed(affs_xy[z], seeds)
-        else:
-            fragments[z] = ndimage.watershed_ift((255.0*affs_xy[z]).astype(np.uint8), seeds)
+        affs_xy = 1.0 - 0.5*(affs[1,z] + affs[2,z])
+        seeds, num_seeds = get_seeds(affs_xy, next_id=next_id, method=seed_method, \
+                                     boundary_thres=boundary_thres, label_nb = label_nb)
+        fragments[z] = mahotas.cwatershed(affs_xy, seeds)
         next_id += num_seeds
 
     return fragments
--- waterz/seg_waterz.py
+++ waterz/seg_waterz.py
@@ -27,7 +27,7 @@ def waterz(
         gt_border = 25/4.0,
         fragments = None,
         fragments_opt = 0,
-        discretize_queue = 256,
+        discretize_queue = 0,
         fragments_mask = None,
         aff_threshold  = [0.0001,0.9999],
         return_seg = True,
@@ -40,11 +40,8 @@ def waterz(
 
     if fragments is None:
         print('initial watershed')
-        if fragments_opt != 0:
-            if fragments_opt == 1: # mahotas
-                fragments = watershed(affs, 'maxima_distance')
-            elif fragments_opt == 2: # scipy
-                fragments = watershed(affs, 'maxima_distance', use_mahotas_watershed = False)
+        if fragments_opt != 0: # mahotas
+            fragments = watershed(affs, 'maxima_distance', label_nb = np.ones([3,3]))
             if fragments_mask is not None:
                 fragments[fragments_mask==False] = 0
 
