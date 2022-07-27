5196c19c95ee1f4c9bcfd91e4e31ecb1a88d3547
Jason Adhinarta <jasonice6@gmail.com>
2022-07-27 11:46:42 -0400

    Add include_dirs into waterz.region_graph extension to fix setup.py installation

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ extensions = [
     Extension(
         'waterz.region_graph',
         sources=['waterz/region_graph.pyx', 'waterz/frontend_region_graph.cpp'],
+        include_dirs=include_dirs,
         language='c++',
         extra_link_args=['-std=c++11'],
         extra_compile_args=['-std=c++11'])]
